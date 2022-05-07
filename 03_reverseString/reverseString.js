fb983580daebaf65df896b1fb565de50d1714580
Iqbal <malik4300@hotmail.com>
2022-05-07 13:43:13 -0500

    Passed all tests for Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    split = string.split("");
+        
+        let joined='';
 
+        for(let i = split.length - 1; i >= 0; i--)
+        {
+            joined += split[i];
+        }
+
+        return joined;
 };
 
 // Do not edit below this line
